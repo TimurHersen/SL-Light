a6dafeb2f38416af4621829b47179feba82c2a3d
TimurHersen <timurhersen@gmail.com>
2022-02-22 12:21:05 +0100

    first commit

---
+++ src/Graph.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+
+
+    HashMap<Node, ArrayList> nodeList = new HashMap<>();
+
+
+    private void readStationsAndTimes(){
+        FileReader fileReader = new FileReader();
+        BufferedReader bf = new BufferedReader();
+    }
+}
